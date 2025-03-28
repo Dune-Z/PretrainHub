6889e1812b10033ea22ab946423edb1071abeb64
Dune-Z <dune.ustc@gmail.com>
2025-03-27 19:30:25 -0500

    change wandb project

--- scripts/1.5B.sh
+++ scripts/1.5B.sh
@@ -3,6 +3,7 @@ bash install.sh
 source pretrain-env/bin/activate
 export HF_TOKEN=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
 export WANDB_API_KEY=d61cd005c38e0e1e27d921c951303410316ac718
+export WANDB_PROJECT=pretrain-hub
 wandb login --relogin $WANDB_API_KEY
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun -m --nnodes=1 --nproc_per_node=8 src.train \
--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,6 +1,8 @@
 export HF_TOKEN=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
 export WANDB_API_KEY=d61cd005c38e0e1e27d921c951303410316ac718
+export WANDB_PROJECT=pretrain-hub
+
 wandb login --relogin $WANDB_API_KEY
-CUDA_VISIBLE_DEVICES=0,1,2,3,8,9 torchrun -m --nnodes=1 --nproc_per_node=6 src.train \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,8 torchrun -m --nnodes=1 --nproc_per_node=8 src.train \
     --config-path ../recipes/runs \
     --config-name debug
