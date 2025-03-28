10e6fdb74863ad615612777fa13396a295a5cc08
Dune-Z <dune.ustc@gmail.com>
2025-03-28 13:16:45 -0500

    revise port

--- scripts/1.5B.sh
+++ scripts/1.5B.sh
@@ -6,6 +6,6 @@ export WANDB_API_KEY=d61cd005c38e0e1e27d921c951303410316ac718
 export WANDB_PROJECT=pretrain-hub
 wandb login --relogin $WANDB_API_KEY
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun -m --nnodes=1 --nproc_per_node=8 src.train \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun -m --nnodes=1 --nproc_per_node=8 --master_port=$PORT1 src.train \
     --config-path ../recipes/runs \
     --config-name 1.5B
